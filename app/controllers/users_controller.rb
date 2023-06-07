ef30ad72e57233894e9b7a714bc6d553e482deb8
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-06-07 05:17:31 +0000

    add ユーザーアクセス制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -6,13 +7,13 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+  @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+  @user = User.find(params[:id])
+  @user.update(user_params)
+  redirect_to user_path(@user.id)
   end
 
 
@@ -21,5 +22,14 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
+
+# ここから追加
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+  end
+  # ここまで追加
 
 end
